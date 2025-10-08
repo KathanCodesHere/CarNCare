23eef20b40905884add26d32431c0326b6eca611
Kathan Sahu <kathan.sahu2023@gmail.com>
2025-10-08 18:42:38 +0530

    Created React Project and added Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h2 className='font-bold  text-4xl'>Welcome to the Car Rental</h2>
+    </div>
+  )
+}
+
+export default App
