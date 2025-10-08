f82b742e62ff92c9b1bf6a98d31c82d36f50795a
Kathan Sahu <kathan.sahu2023@gmail.com>
2025-10-08 20:32:19 +0530

    Project structure updated

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h2 className='font-bold  text-4xl'>Welcome to the Car Rental home</h2>
+    </div>
+  )
+}
+
+export default Home
