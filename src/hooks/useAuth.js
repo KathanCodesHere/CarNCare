41803e2eca9bc23a0734673d604ec2be921f9b58
Kathan Sahu <kathan.sahu2023@gmail.com>
2025-11-03 17:12:22 +0530

    changes in auth

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -53,6 +53,28 @@ export const useAuth = () => {
       console.error("Logout failed:", err);
     }
   };
+  const verifyOtp = async (userData) => {
+  try {
+    setLoading(true);
+    setError(null); // clear any previous error
+
+    const data = await authService.verifyOtp(userData);
+    return data; // you can navigate or show success toast here
+  } catch (err) {
+    console.error("Signup error:", err);
+
+    // Normalize different types of possible errors
+    const message =
+      err?.response?.data?.message || // API error message
+      err?.message ||                // Network or Axios message
+      "Something went wrong during signup";
+
+    setError(message); // store for UI display
+    return null; // optional: helps caller know signup failed
+  } finally {
+    setLoading(false);
+  }
+};
 
   return { login, signup, logout, loading, error };
 };
